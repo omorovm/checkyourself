8edf74a6787bdcb01d3c4584c03dbdc95ae6d3fb
omorovm <omorovmeder23@gmail.com>
2023-11-10 21:07:57 +0600

    firs commit

---
+++ tasks.py
@@ -0,0 +1,29 @@
+# first task
+class AutoMixin:
+    def ride(self):
+        print("Riding on a ground")
+
+class BoatMixin:
+    def swim(self):
+        print("Floating on water")
+
+class Amphibian(AutoMixin, BoatMixin):
+    pass
+
+obj = Amphibian()
+obj.ride()
+obj.swim()
+
+# second task
+
+class ContactList(list): 
+    def __init__(self, list_): 
+        self.list_ = list_ 
+    def search_by_name(self, name): 
+        a = [] 
+        for i in self.list_: 
+            if name in i: a.append(i) 
+            return a 
+
+all_contacts = ContactList(['Ivan', 'Maris', 'Olga', 'Ivan Olya', 'Olya Ivan', 'ivan']) 
+print(all_contacts.search_by_name('Olya'))
